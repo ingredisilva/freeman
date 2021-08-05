c308a1add43a1927fe94794c713a2c79a4ef331f
Ingredi Silva <ingredisilva.dev@gmail.com>
2021-08-05 14:09:37 -0300

    Correções, Modal, Local Storage e mais

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # freeman
+
 Freeman is a functionality to manage the life of a Freelancer.
+
+O que pratiquei nessa edição do Discover da Rocketseat:
+
+1. Para usar algo de dentro de uma função é necessário usar o 'return';
+2. Como lidar com dinheiro: como se a casa decimal estivesse lá;
+3. Formatação de Moeda;
+4. Format Currency
+5. String(value).replace
+6. Manipulação de Arrays, com Push, Splice
+7. Como fazer um Modal;
+8. Local Storage;
+
+!Em Construção!
+
+[Freeman, um App de controle financeiro](./assets/freeman.png "Freeman")
--- app.js
+++ app.js
@@ -2,131 +2,212 @@ const Modal = {
   open() {
     // abrir modal
     // adicionar a class active ao modal
-    document
-    .querySelector(".modal-overlay")
-    .classList.add("active");
+    document.querySelector(".modal-overlay").classList.add("active");
   },
+
   close() {
-    // fechar o modal
-    // remover a class active do modal
-    document
-    .querySelector(".modal-overlay")
-    .classList.remove("active");
+    document.querySelector(".modal-overlay").classList.remove("active");
   },
 };
 
-const transactions = [
-  {
-    id: 1,
-    description: "Luz",
-    amount: -50000,
-    date: "23/2/2021",
+const Storage = {
+  get() {
+    return JSON.parse(localStorage.getItem("freeman:transactions")) || [];
   },
 
-  {
-    id: 2,
-    description: "Luz",
-    amount: -50000,
-    date: "23/2/2021",
+  set() {
+    localStorage.setItem("freeman:transactions", JSON.stringify(transactions));
   },
-
-  {
-    id: 1,
-    description: "Luz",
-    amount: -50000,
-    date: "23/2/2021",
-  },
-];
+};
 
 const Transaction = {
+  all: Storage.get(),
+
+  add(transaction) {
+    Transaction.all.push(transaction);
+
+    App.reload();
+  },
+
+  remove(index) {
+    Transaction.all.splice(index, 1);
+
+    App.reload();
+  },
+
   incomes() {
     let income = 0;
-    // pegar todas as transações
-    transaction.forEach(transaction => {
-      if( transaction.amount > 0) {
+    Transactions.all.forEach((transaction) => {
+      if (transaction.amount > 0) {
         income += transaction.amount;
       }
-    })
-    // para cada transação, se é maior que zero
-    // somar a uma variavel e retornar a variavel
+    });
     return income;
-    // somar as entradas
   },
+
   expenses() {
     let expense = 0;
-     transaction.forEach(transaction => {
-      if( transaction.amount < 0) {
-        income += transaction.amount;
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount < 0) {
+        expense += transaction.amount;
       }
-      return expense;
+    });
+    return expense;
   },
+
   total() {
-    return Transaction.income() + Transaction.expenses();
-    // entradas - saídas
-  }
-}
+    return Transaction.incomes() + Transaction.expenses();
+  },
+};
+
+//Substituir os dados do HTML com os dados do JS
 
 const DOM = {
-  transactionsContainer: document.querySelector('#data-table tbody'), 
+  transactionsContainer: document.querySelector("#data-table tbody"),
 
   addTransaction(transaction, index) {
-    const tr = document.createElement("tr")
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
 
-    DOM.transactionsContainer.appendChild('tr')
-
-},
-    innerHTMLTransaction(transaction) {
-const CSSclass = transaction.amount > 0 ? "income" :
-      "expense"
-
-      const amount = Utils.formatCurrency(transaction.amount)
-
-      const html = `
-          <tr>
-                <td class="description">${transaction.description}</td>
-                <td class=${CSSclass}>${amont}</td>
-                <td class="date">${transaction.date}</td>
-                <td>
-                  <img src="assets/minus.svg" alt="Remover transação">
-                </td>
-              </tr>`
-
-              return html
+    DOM.transactionsContainer.appendChild(tr);
   },
 
-  updateBalance(){
-    document
-    .getElementById('incomeDisplay')
-    .innerHTML = Transaction.incomes()
-    document
-    .getElementById('expenseDisplay')
-    .innerHTML = Transaction.expenses()
-    document
-    .getElementById('totalDisplay')
-    .innerHTML = Transaction.total 
-  }
-},
+  innerHTMLTransaction(transaction, index) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+    const amount = Utils.formatCurrency(transaction.amount);
+
+    const html = `
+    <td class="description">${transaction.description}</td>
+    <td class="${CSSclass}">${amount}"</td>
+    <td class="date">${transaction.date}"</td>
+    <td>
+    <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação" />
+    </td>
+   `;
+
+    return html;
+  },
+
+  upadateBalance() {
+    document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.incomes()
+    );
+    document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.expenses()
+    );
+    document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.total()
+    );
+  },
+
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = "";
+  },
+};
+
+//transformar string to number, Format Currency tbm
 
 const Utils = {
+  formatAmount(value) {
+    value = Number(value.replace(/\,\./g, "")) * 100;
+
+    return value;
+  },
+
+  formatDate(date) {
+    const splittedDate = date.split("-");
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  },
+
   formatCurrency(value) {
-    const signal = Number(value) < 0 ? "-" : ""
+    const signal = Number(value) < 0 ? "-" : "";
 
-    value = String(value).replace(/\D/g, "")
+    value = String(value).replace(/\D/g, "");
 
-    value = Number(value) / 100
+    value = Number(value) / 100;
 
-    value = value.toLocaleString("pt-BR" {
+    value = value.toLocaleString("pt-BR", {
       style: "currency",
-      currency: "BRL"
-    })
+      currency: "BRL",
+    });
 
-    return signal + value
-  }
-}
+    return signal + value;
+  },
+};
 
-transactions.forEach(function(transaction) {
+const Form = {
+  description: document.querySelector("input#description"),
+  amount: document.querySelector("input#amount"),
+  date: document.querySelector("input#date"),
 
-})
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+    };
+  },
 
-//DOM.updateBalance
+  validateFields() {
+    const { description, amount, date } = Form.getValues();
+
+    if (
+      description.trim() === "" ||
+      amount.trim() === "" ||
+      date.trim() === ""
+    ) {
+      throw new Error("Por favor, preencha todos os campos");
+    }
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues();
+
+    amount = Utils.formatAmount(amount);
+
+    date = Utils.formatDate(date);
+    return {
+      description,
+      amount,
+      date,
+    };
+  },
+
+  clearFields() {
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
+  },
+
+  submit(event) {
+    event.preventDefault();
+
+    try {
+      Form.validateFields();
+      const transaction = Form.formatValues();
+      Transaction.add(transaction);
+      Form.clearFields();
+      Modal.close();
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+};
+
+const App = {
+  init() {
+    Transaction.all.forEach(DOM.addTransaction);
+
+    DOM.updateBalance();
+
+    Storage.set(Transaction.all);
+  },
+  reload() {
+    DOM.clearTransactions();
+    App.init();
+  },
+};
+
+App.init();
